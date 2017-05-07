da13f57eda9ac58224c5f25e66aef6e5b0bfc459
Imran Panjwani <imranpanjwani00@gmail.com>
2017-05-07 14:37:04 +0530

    create initial structure of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './recipe-book/recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingComponent,
+    RecipeBookComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent
   ],
   imports: [
     BrowserModule,
