7e1408ef8306c23dff1e810d53a321c9827024e0
Imran Panjwani <imranpanjwani00@gmail.com>
2017-06-03 16:21:06 +0530

    structure recipe-book with static data

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  public recipes : Recipe[] = [
+    new Recipe('New Recipe','Tasty recipe','https://c1.staticflickr.com/6/5683/20156435453_293cf9dde6_b.jpg'),
+    new Recipe('New Recipe','Tasty recipe','https://c1.staticflickr.com/6/5683/20156435453_293cf9dde6_b.jpg'),
+  ];
   constructor() { }
 
   ngOnInit() {
